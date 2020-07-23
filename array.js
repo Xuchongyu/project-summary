54aa9592abc7d669d592212073894d0553be29b8
opera <opera@operadeMacBook-Pro.local>
2020-07-23 15:07:25 +0800

    。。。

---
+++ array.js
@@ -0,0 +1,66 @@
+// const array = []
+// const pushItem = array.push(1, 2) //朴实push 方法可以改变元数组 向数组的后面添加一个元素 返回添加的元素个数
+// console.log(array, pushItem)
+// const popItem = array.pop() // pop方法 方法也可以改变数组 截取数组最后一位元素 返回被截取的元素
+// console.log(popItem, array)
+// const shiftItem = array.shift() //shift 方法 可以改变数组 截取数组第一个元素 返回被截取的第一个元素
+// console.log(array, shiftItem)
+// const unshiftItem = array.unshift(0, 1) //unshift 方法 可以改变原数组 向数组中开始的位置添加个元素，返回添加的元素个数
+// console.log(unshiftItem, array)
+// 数组去重
+// console.log([...new Set([1, 1, 2, 3, 3, 3, 3, 4, 4, 4, 9])])
+// 对象数组去重
+// function unique(arr) {
+//   let unique = {}
+//   arr.forEach(function (item) {
+//     unique[JSON.stringify(item)] = item //键名不会重复
+//   })
+//   arr = Object.keys(unique).map(function (u) {
+//     //Object.keys()返回对象的所有键值组成的数组，map方法是一个遍历方法，返回遍历结果组成的数组.将unique对象的键名还原成对象数组
+//     return JSON.parse(u)
+//   })
+//   return arr
+// }
+// const oA = [
+//   {
+//     name: '!23',
+//   },
+//   {
+//     name: '!23',
+//   },
+//   {
+//     name: '!23',
+//   },
+// ]
+// console.log(unique(oA))
+const array = [1, 2, 3, 4, 5]
+// const newArray = array.reverse()
+// console.log(newArray)
+// const sortArray = array.sort((a, b) => {
+//   // if (a > b) return -1
+//   // else if (a < b) return 1
+//   // else return 0
+//   // return a - b
+//   return b - a
+// })
+// const newArray = array.splice(0, 1)
+// console.log(newArray, array)
+// const str = '12345'
+// console.log(array.split(0, 1))
+/**
+ * reduce
+ */
+// const maxValue = array.reduce((prev, item) => {
+//   console.log(prev, item)
+//   return prev > item ? prev : item
+// })
+// const maxValue = Math.max.apply(null, array)
+// console.log(maxValue)
+const returnFucntion = () => {
+  // if (false) return true
+}
+function handle() {
+  // returnFucntion()
+  console.log(123)
+}
+handle()
---
+++ js高级程序设计.js
@@ -0,0 +1,221 @@
+// if (true) {
+//   var color = 'blue'
+// }
+// console.log(color)
+// for (var i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+// console.log(i)
+// for (let i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+// console.log(i)
+// function add(num1, num2) {
+//   var num3 = num1 + num2
+//   return num3
+// }
+// const result = add(10, 20)
+// console.log(result)
+// console.log(num3) // ReferenceError: num3 is not defined
+// 'use strict'
+// function add(num1, num2) {
+//   num3 = num1 + num2
+//   return num3
+// }
+// const result = add(10, 20)
+// console.log(result)
+// console.log(num3) 在非严格模式的情况下 num3 默认会被创建到全局环境中，在严格模式中不可以
+// test findIndex
+// const arr = [1, 2, 3, 45]
+// const index = arr.findIndex((item) => item === 0)
+// console.log(index)
+// console.log(new Array('red', 'blur', 'green')) // 创建一个 red blue green的数组
+// console.log(new Array(3))                      //创建一个包含3的数组项
+// const arr = [1, 2]
+// arr.splice(0, 0, 0)
+// const arr1 = arr.unshift(0)
+// const arr1 = arr.shift(1)
+// console.log(arr)
+// const colors = ['red', 'blur', 'green']
+// const colors = [1, 2, 3, 4]
+// colors[99] = 'black'
+// console.log(colors)
+// console.log(colors instanceof Array)
+// console.log(Array.isArray(colors))
+// console.log(Array.toString())
+// console.log(colors.valueOf())
+// console.log(colors.toLocaleString())
+// const person = {
+//   toLocaleString() {
+//     return 'toLocaleString 1'
+//   },
+//   toString() {
+//     return 'toString 1'
+//   },
+// }
+// const person2 = {
+//   toLocaleString() {
+//     return 'toLocaleString 2'
+//   },
+//   toString() {
+//     return 'toString 2'
+//   },
+// }
+// const array = [person, person2]
+// console.log(array)
+// console.log(array.toLocaleString())
+// console.log(array.toString())
+// const str = 'aaaa1212121212112212112'
+// const str2Arr = str.split('')
+
+// let startIndex = 0
+// let lastStr = ''
+// str2Arr.forEach((item, index) => {
+//   const text = str.substr(startIndex, index)
+//   const spanNode = document.createElement('span')
+//   spanNode.innerHTML = text
+//   spanNode.className = 'getTextWidth'
+//   document.querySelector('body').appendChild(spanNode)
+//   const wdith = document.querySelector('.getTextWidth').offsetWidth
+//   document.querySelector('.getTextWidth').remove()
+//   if (wdith > 10) {
+//     startIndex = index
+//     lastStr += text + '\n'
+//   }
+// })
+// console.log(lastStr)
+// const arr = [1, 2, 3, 4, 5, 666]
+// const maxVaule = Math.max.apply(null, arr)
+// console.maxValue(maxVaule)
+// const maxValue = arr.reduce((x, y, z) => {
+//   return x > y ? x : y
+// }, 0)
+// console.log(maxValue)
+const setData = new Set([1, 2, 3, 4, 5, 5])
+// setData.forEach((item) => {
+//   console.log(item)
+// })
+// set转数组
+// const arrayFromData = Array.from(setData)
+// setData.add(6).add(7)
+// setData.delete(5)
+// setData.clear()
+// console.log(setData, setData.size)
+// console.log(setData.has(1))
+
+// console.log(arrayFromData)
+
+/**
+ * es5数组去重
+ */
+// var array = [1, '1', 1, 2, 3, 2, 4]
+// var tmpObj = {}
+// var result = []
+// array.forEach(function (a) {
+//   var key = typeof a + a
+//   console.log(!tmpObj[key])
+//   if (!tmpObj[key]) {
+//     tmpObj[key] = true
+//     console.log(tmpObj)
+//     result.push(a)
+//   }
+// })
+// console.log(result)
+// const mapData = new Map([[{ a: 1234 }, 2]])
+// mapData.set({ a: 1, b: 2 }, 'value1')
+// console.log(mapData)
+// function P() {}
+// P.prototype.name = '徐崇玉'
+// const p1 = new P()
+// const status = p1.hasOwnProperty('name')
+// console.log(status)
+// console.log(P.prototype.constructor === P)
+// console.log(P.prototype.isPrototypeOf(p1)) //判断P是不是p1的构造函数 或者判断p1是不是P的实例
+// const p2 = new P()
+// p2.name = 'selfName'
+// delete p2.name
+// console.log(p1.name, p2.name)
+// 给后端返回的数据排序
+const array = [
+    {
+        key: 'esms',
+        href: '/esms',
+        name: 'ES管理平台',
+    },
+    {
+        key: 'index',
+        href: '/index.html',
+        name: '数据开发',
+    },
+    {
+        key: 'metis',
+        href: '/metis.html',
+        name: '实时开发',
+    },
+    {
+        key: 'dsManager',
+        href: '/dsManager.html',
+        name: '数据服务',
+    },
+    {
+        key: 'dataSync',
+        href: '/dataSync.html',
+        name: '数据同步',
+    },
+    {
+        key: 'dqc',
+        href: '/dqc.html',
+        name: '数据质量',
+    },
+    {
+        key: 'map',
+        href: '/map.html',
+        name: '数据地图',
+    },
+    {
+        key: 'assets',
+        href: '/assets.html',
+        name: '数据资产',
+    },
+    {
+        key: 'jobops',
+        href: '/jobops.html',
+        name: '任务运维',
+    },
+    {
+        key: 'access',
+        name: '权限管理',
+        children: [
+            {
+                key: 'auth',
+                href: '/auth.html',
+                name: '数据权限',
+            },
+            {
+                key: 'mastiff',
+                href: '/mastiff.html',
+                name: '功能权限',
+            },
+        ],
+    },
+    {
+        key: 'measure',
+        href: '/measure.html',
+        name: '指标中心',
+    },
+    {
+        key: 'adminManager',
+        href: '/adminManager.html',
+        name: '高级管理',
+    },
+
+    {
+        key: 'billing-web',
+        href: '/bigdata/billing-web',
+        name: '计费系统',
+    },
+]
+const sortArray = array.sort((a, b) => {
+    return a.name.length - b.name.length
+})
+console.log(sortArray)
